de6bf8e02b71fa733b6f3b4ef6d22de5fe18486b
Victor Canto <iamvictorcanto@gmail.com>
2022-04-20 17:07:40 -0300

    feat: created Shop component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
-
-const Shop = () => <h1>Shop</h1>;
+import Shop from './routes/shop/shop.component';
 
 const App = () => (
   <Routes>
