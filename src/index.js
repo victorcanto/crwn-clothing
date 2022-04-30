1179c5f5dfc532b0a2ce9e30860f673827b5327a
Victor Canto <iamvictorcanto@gmail.com>
2022-04-30 16:04:48 -0300

    feat: created shop page and adaptation of related components

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { UserProvider } from './contexts/user.context';
 import reportWebVitals from './reportWebVitals';
 
 import './index.scss';
-import { ProductsProvider } from './contexts/products.context';
+import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,11 +16,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
