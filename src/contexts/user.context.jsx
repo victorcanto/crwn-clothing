5044eeac36933ef906bfc88e770e0c06a3f104ab
Victor Canto <iamvictorcanto@gmail.com>
2022-05-07 03:10:30 -0300

    fea(user.context)t: used createAction helper

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -3,6 +3,7 @@ import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
 } from '../utils/firebase/firebase.utils';
+import { createAction } from '../utils/reducer/reducer.utils';
 
 export const UserContext = createContext({
   currentUser: null,
@@ -34,7 +35,7 @@ export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
 
   const value = { currentUser, setCurrentUser };
