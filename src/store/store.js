d46b161722d259cad750518572200bcdec96ef55
Victor Canto <iamvictorcanto@gmail.com>
2022-05-17 00:03:05 -0300

    feat: created thunks and updated actions, types and reducer and persistConfig

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
 import logger from './middleware/logger';
 
 import { rootReducer } from './root-reducer';
@@ -8,14 +9,15 @@ import { rootReducer } from './root-reducer';
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user'],
+  whitelist: ['cart'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(
-  Boolean
-);
+const middlewares = [
+  process.env.NODE_ENV !== 'production' && logger,
+  thunk,
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
