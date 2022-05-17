52d603b4eac5b6932cf6087e3d2025eb7ab94212
Victor Canto <iamvictorcanto@gmail.com>
2022-05-17 00:06:13 -0300

    feat: rename and used fetchCategoriesAsync in shop

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,19 +4,13 @@ import { useDispatch } from 'react-redux';
 
 import Category from '../../components/category/category.component';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/categories.action';
+import { fetchCategoriesAsync } from '../../store/categories/categories.action';
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategories = async () => {
-      const categoriesArray = await getCategoriesAndDocuments('categories');
-      dispatch(setCategories(categoriesArray));
-    };
-
-    getCategories();
+    dispatch(fetchCategoriesAsync());
   }, []);
 
   return (
--- src/store/categories/categories.action.js
+++ src/store/categories/categories.action.js
@@ -14,7 +14,7 @@ export const fetchCategoriesSuccess = (categoriesArray) =>
 export const fetchCategoriesFailure = (error) =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
 
-export const fetchCategoriesStartAsync = () => async (dispatch) => {
+export const fetchCategoriesAsync = () => async (dispatch) => {
   dispatch(fetchCategoriesStart());
 
   try {
