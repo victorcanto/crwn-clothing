67b131d67386a566bd40f5e59b331cd12d84cdfc
Victor Canto <iamvictorcanto@gmail.com>
2022-06-25 02:17:23 -0300

    feat(categories.saga): added new categories sagas for get categories from api

--- src/store/categories/categories.action.js
+++ src/store/categories/categories.action.js
@@ -1,4 +1,3 @@
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 import { createAction } from '../../utils/reducer/reducer.utils';
 import { CATEGORIES_ACTION_TYPES } from './categories.types';
 
@@ -11,16 +10,5 @@ export const fetchCategoriesSuccess = (categoriesArray) =>
     categoriesArray
   );
 
-export const fetchCategoriesFailure = (error) =>
+export const fetchCategoriesFailed = (error) =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
-
-export const fetchCategoriesAsync = () => async (dispatch) => {
-  dispatch(fetchCategoriesStart());
-
-  try {
-    const categoriesArray = await getCategoriesAndDocuments('categories');
-    dispatch(fetchCategoriesSuccess(categoriesArray));
-  } catch (error) {
-    dispatch(fetchCategoriesFailure(error));
-  }
-};
