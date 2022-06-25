adee58ed84b85521cbc901595c69ae35d6f13eb4
Victor Canto <iamvictorcanto@gmail.com>
2022-06-25 02:18:06 -0300

    feat(categories.reducer): added new cases

--- src/store/categories/categories.reducer.js
+++ src/store/categories/categories.reducer.js
@@ -15,17 +15,9 @@ export const categoriesReducer = (
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
       return { ...state, isLoading: true };
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return {
-        ...state,
-        categories: payload,
-        isLoading: false,
-      };
+      return { ...state, categories: payload, isLoading: false };
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return {
-        ...state,
-        error: payload,
-        isLoading: false,
-      };
+      return { ...state, error: payload, isLoading: false };
     default:
       return state;
   }
