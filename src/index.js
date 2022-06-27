c16d224240a6b3c707f213382d76bc3a5c9c444e
Victor Canto <iamvictorcanto@gmail.com>
2022-06-26 21:24:57 -0300

    feat: provides Elements from stripe to App

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './store/store';
+
+import { Elements } from '@stripe/react-stripe-js';
 
 import App from './App';
+import { store, persistor } from './store/store';
+import { stripePromise } from './utils/stripe/stripe.utils';
 import reportWebVitals from './reportWebVitals';
 
 import './index.scss';
@@ -18,7 +21,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
