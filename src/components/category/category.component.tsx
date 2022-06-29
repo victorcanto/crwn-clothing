0c343546adf26afc504f74997f22f43d55a4c4f8
Victor Canto <iamvictorcanto@gmail.com>
2022-06-29 15:06:57 -0300

    feat: typing Category component

--- src/components/category/category.component.tsx
+++ src/components/category/category.component.tsx
@@ -10,8 +10,14 @@ import ProductCard from '../product-card/product-card.component';
 import Spinner from '../spinner/spinner.component';
 import { Title, CategoryContainer } from './category.styles';
 
+type CategoryRouteParams = {
+  category: string;
+};
+
 const Category = () => {
-  const { category } = useParams();
+  const { category } = useParams<
+    keyof CategoryRouteParams
+  >() as CategoryRouteParams;
   const categoriesMap = useSelector(selectCategoriesMap);
   const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
