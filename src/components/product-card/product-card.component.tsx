e200d3b42cc9293945cd5b1ac9e4c678165d4fb1
Victor Canto <iamvictorcanto@gmail.com>
2022-06-29 17:21:14 -0300

    feat: typing ProductCard component

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -1,6 +1,8 @@
+import { FC } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addItemToCart } from '../../store/cart/cart.action';
 import { selectCartItems } from '../../store/cart/cart.selector';
+import { CategoryItem } from '../../store/categories/categories.types';
 
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
@@ -11,7 +13,11 @@ import {
   Price,
 } from './product-card.styles';
 
-const ProductCard = ({ product }) => {
+type ProductCardProps = {
+  product: CategoryItem;
+};
+
+const ProductCard: FC<ProductCardProps> = ({ product }) => {
   const { name, imageUrl, price } = product;
   const dispatch = useDispatch();
   const cartItems = useSelector(selectCartItems);
