f8be8d9be970012234fbd4a20ebcdb52ba11e763
Victor Canto <iamvictorcanto@gmail.com>
2022-07-02 00:22:26 -0300

    feat: converted to pwa

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import { store, persistor } from './store/store';
 import { stripePromise } from './utils/stripe/stripe.utils';
 import reportWebVitals from './reportWebVitals';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -34,3 +36,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
